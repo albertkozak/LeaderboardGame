a7cb7ef8fd9011cd70b74740b426a396e4eecba0
Albert Kozak <albert.kozak@live.com>
2020-02-25 12:57:16 -0800

    Edited app game flow

--- app/src/main/java/com/albertkozak/leaderboardgame/AuthActivity.kt
+++ app/src/main/java/com/albertkozak/leaderboardgame/AuthActivity.kt
@@ -31,7 +31,7 @@ class AuthActivity: AppCompatActivity() {
                         .addOnCompleteListener(this) { task ->
                             if (task.isSuccessful) {
                                 val user = auth.currentUser
-                                startActivity(Intent(this, GameActivity::class.java))
+                                startActivity(Intent(this, LeaderboardActivity::class.java))
                             }
 
                             // Handle error here
@@ -43,7 +43,7 @@ class AuthActivity: AppCompatActivity() {
                         .addOnCompleteListener(this) { task ->
                             if (task.isSuccessful) {
                                 val user = auth.currentUser
-                                startActivity(Intent(this, GameActivity::class.java))
+                                startActivity(Intent(this, LeaderboardActivity::class.java))
                             }
                             // Handle error here
                         }
