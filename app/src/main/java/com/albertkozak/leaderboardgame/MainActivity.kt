fdbd344222f182ff441597d66161b97508aca4c9
Albert Kozak <albert.kozak@live.com>
2020-02-25 10:25:26 -0800

    Connected to Firebase and tested connection, enabled authentication, created login screen

--- app/src/main/java/com/albertkozak/leaderboardgame/MainActivity.kt
+++ app/src/main/java/com/albertkozak/leaderboardgame/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.albertkozak.leaderboardgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+        myRef.setValue("Update")
     }
 }
